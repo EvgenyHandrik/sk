ccdbc4a9110197e9723af9649ba9464d7144be93
Evgeny Handrik <evgeny.handrik@yandex.ru>
2019-02-01 14:35:48 +0500

    верстка страницы combo

--- build/js/page-combo.js
+++ build/js/page-combo.js
@@ -7,13 +7,61 @@
 			$dish = $button.closest('.combo-dish');
 			$brief = $dish.find('.combo-dish-brief');
 
-			if ($brief.hasClass('combo-dish-brief--isHidden')) {
-				$brief.removeClass('combo-dish-brief--isHidden');
-				$button.addClass('combo-dish-brief-toggle--isActive');
+			if ($brief.hasClass('combo-dish-brief--is-hidden')) {
+				$brief.removeClass('combo-dish-brief--is-hidden');
+				$button.addClass('combo-dish-brief-toggle--is-active');
 			} else {
-				$brief.addClass('combo-dish-brief--isHidden');
-				$button.removeClass('combo-dish-brief-toggle--isActive');
+				$brief.addClass('combo-dish-brief--is-hidden');
+				$button.removeClass('combo-dish-brief-toggle--is-active');
 			}
 		});
+
+}());
+
+// Combo tab
+;(function() {
+
+	var $tab = $('.js-combo-tab');
+	var $buttons = $tab.find('.combo-tab-nav__button');
+	var $menuItems = $tab.find('.combo-tab-nav__item');
+	var $contents = $tab.find('.combo-tab-content__item');
+
+	$tab.on('click.combo-tab', '.combo-tab-nav__button', function() {
+		var $button = $(this);
+		var index = $buttons.index($button);
+		var $item = $menuItems.eq(index);
+		var $content = $contents.eq(index);
+		
+		if (!$item.hasClass('combo-tab-nav__item--active')) {
+			$menuItems.filter('.combo-tab-nav__item--active')
+				.removeClass('combo-tab-nav__item--active');
+			$contents.filter('.combo-tab-content__item--active')
+				.removeClass('combo-tab-content__item--active');
+			
+			$item.addClass('combo-tab-nav__item--active');
+			$content.addClass('combo-tab-content__item--active');
+		} 
+	});
+
+}());
+
+// Combo c-info
+;(function() {
+
+	var $box = $('.js-combo-c-info');
+	
+	$box.on('click.combo-c-info', '.combo-c-info__button', function() {
+		var $button = $(this);
+
+		if ($button.hasClass('combo-c-info__button--short')) {
+			$box
+				.removeClass('combo-c-info--full')
+				.addClass('combo-c-info--short');
+		} else {
+			$box
+				.removeClass('combo-c-info--short')
+				.addClass('combo-c-info--full');
+		}
+	});
 
 }());
