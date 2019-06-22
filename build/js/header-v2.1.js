d4c3ddd3a358f691db76e92ada9b00306f126aa9
Evgeny Handrik <evgeny.handrik@yandex.ru>
2019-06-22 15:24:46 +0500

    добавление функциональности: показать/скрыть h-menu-v2-main items

--- build/js/header-v2.1.js
+++ build/js/header-v2.1.js
@@ -1,4 +1,4 @@
-(function() {
+;(function() {
 
 	var $window = $(window);
 	var $header = $('.header-v2');
@@ -92,6 +92,98 @@
 			.removeClass('h-menu-toggle--close');
 		$nav
 			.removeClass('h-nav--open'); // v2.2
+	}
+
+}());
+
+// h-menu-v2 more
+(function() {
+	var $container = $('.js-h-menu-v2-more');
+	var $main;
+	var $mainItems;
+	var resizeTimer = 0;
+
+	if ($container.length) {
+		$main = $container.find('.h-menu-v2-main');
+		$mainItems = $main.find('.h-menu-v2-item');
+
+		addHandlers();
+	}
+
+	function checkMain() {
+		var mainWidth = $main.innerWidth();
+		var itemsWidth = getMainItemsWidth();
+		
+		var $hiddenItems = $mainItems.filter('.h-menu-v2-item--hidden');
+
+		if (itemsWidth > mainWidth) {
+			hideMainItems(mainWidth, itemsWidth);
+		} else if ($hiddenItems.length) {
+			showMainItems($hiddenItems, mainWidth);
+		}
+	}
+
+	function showMainItems($hiddenItems, mainWidth) {
+		$hiddenItems = $hiddenItems || $items.filter('.h-menu-v2-item--hidden');		
+		mainWidth = mainWidth || $main.innerWidth();
+		
+		$hiddenItems.each(function() {
+			var $hiddenItem = $(this);
+			var hiddenItemWidth = $hiddenItem.innerWidth();
+			var itemsWidth = getMainItemsWidth();
+			var freeSpace = mainWidth - itemsWidth;
+
+			if (freeSpace >= hiddenItemWidth) {
+				showMainItem($hiddenItem);
+			} else {
+				return false;
+			}
+		});
+	}
+
+	function showMainItem($mainItem) {
+		$mainItem = $mainItem || $items.filter('.h-menu-v2-item--hidden').first();
+		$mainItem
+			.removeClass('h-menu-v2-item--hidden');
+		
+		console.log('-- main item was shown');
+	}
+
+	function hideMainItems(mainWidth, itemsWidth) {
+		mainWidth = mainWidth || $main.innerWidth();
+		itemsWidth = itemsWidth || getMainItemsWidth();
+
+		if (itemsWidth > mainWidth) {
+			hideMainItem();
+			hideMainItems(mainWidth);
+		}
+	}
+
+	function hideMainItem() {
+		$mainItems.not('.h-menu-v2-item--hidden').last()
+			.addClass('h-menu-v2-item--hidden');
+		
+		console.log('-- main item was hidden');
+	}
+
+	function getMainItemsWidth() {
+		var itemsWidth = 0;
+		$mainItems.not('.h-menu-v2-item--hidden')
+			.each(function() {
+				itemsWidth += $(this).innerWidth();
+			});
+
+		return itemsWidth;
+	}
+
+	function addHandlers() {
+		$(window)
+			.on('resize.h-menu-v2', function() {
+				clearTimeout(resizeTimer);
+				resizeTimer = setTimeout(function() {
+					checkMain();
+				}, 300);
+			});
 	}
 
 }());
