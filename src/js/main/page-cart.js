87d7a3f5e71aec59f8eeaa4e37afca174c968ffb
Evgeny Handrik <evgeny.handrik@yandex.ru>
2019-06-24 15:02:09 +0500

    верстка crt2-recommended v2

---
+++ src/js/main/page-cart.js
@@ -0,0 +1,26 @@
+;(function() {
+
+	var $container = $('.js-crt2-recommended-more');
+	var $scroll;
+	var scrollLeftSize = (140 + 10) * 4;
+
+	if ($container.length) {
+		$scroll = $container.find('.crt2-recommended-scroll');
+
+		$container.on('click.more-toggle', '.crt2-recommended-more-toggle', function() {
+			$container.removeClass('crt2-recommended--more');
+
+			$scroll
+				.off('scroll.more-scroll')
+				.delay(500).animate({
+					scrollLeft: scrollLeftSize
+				}, 400, 'linear');
+		});
+
+		$scroll
+			.one('scroll.more-scroll', function() {
+				$container.removeClass('crt2-recommended--more');
+			});
+	}
+
+}());
